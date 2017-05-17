381b8d168c8d2a538ef2444032f859b4e1eecb21
youxinyu <1015701481@qq.com>
2017-05-17 21:38:09 +0800

    Update Top-N

--- top_n.py
+++ top_n.py
@@ -1,29 +1,38 @@
+ #! /usr/bin/env python
+ # coding=utf-8
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 import numpy as np
+import sys
 
-stopWords = stopwords.words('english')
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# stopWords = stopwords.words('english')
+
+stopWords = [",", "?", "、", "。", "“", "”", "《", "》", "！", "，", "：", "；", "？",
+"的","了","在","是","我","有","和","就","不","人","都","一","一个","上","也","很","到","说","要","去","你","会","着","没有","看","好","自己","这"]
 vectorizer = CountVectorizer(stop_words = stopWords)
 transformer = TfidfTransformer()
 
-print "reading topics from 11/19"
-with open('topic_list-11-21.txt') as f:
+print "reading topics from 05/16"
+with open('topic_list-5-16.txt') as f:
   content = f.readlines()
   for topic in content:
-    print "\n"
+    print "\n话题:"
     print topic.rstrip()
     train_set = []
-    with open('data/'+topic.rstrip()+'.txt') as data:
+    with open('weiboData/'+topic.rstrip()+'.txt') as data:
       for tweet in data.readlines():
         train_set.append(tweet)
     trainVectorizerArray = vectorizer.fit_transform(train_set).toarray()
     transformer.fit(trainVectorizerArray)
     sums = transformer.transform(trainVectorizerArray).toarray().sum(1)
     sorted_indices = np.argsort(sums)
-    print "first"
+    print "First"
     print train_set[sorted_indices[-1]]
-    print "second"
+    print "Second"
     print train_set[sorted_indices[-2]]
-    print "third"
+    print "Third"
     print train_set[sorted_indices[-3]]
