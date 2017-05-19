f50ef449cd29bfdddd84ef8619d8bf8b899d74a8
youxinyu <1015701481@qq.com>
2017-05-19 14:31:20 +0800

    TF-IDF for Chinese

--- tf-idf.py
+++ tf-idf.py
@@ -3,6 +3,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from nltk.corpus import stopwords
+from textrank4zh import Segmentation
+import codecs
 import numpy as np
 import sys
 import os
@@ -18,62 +20,57 @@ vectorizer = CountVectorizer(stop_words = stopWords)
 transformer = TfidfTransformer()
 
 topic_name = ""
-
-# with open('topic_list-11-21.txt') as f:
-  # content = f.readlines()
-  # for topic in content:
-print "\n"
-# print topic.rstrip()
 train_set = []
 # with open('data/'+topic.rstrip()+'.txt') as data:
 print "reading topics from 05/16"
 with open('topic_list-5-16.txt') as f:
-  content = f.readlines()
-  for topic in content:
-    print "\n话题:"
-    topic_name = topic.rstrip()
-    print topic.rstrip()
-    train_set = []
-    with open('weiboData/'+topic.rstrip()+'.txt') as data:
-    # with open('data/'+'#Blackfish'+'.txt') as data:
-	  for tweet in data.readlines():
-	    train_set.append(tweet)
-    trainVectorizerArray = vectorizer.fit_transform(train_set).toarray()
-    transformer.fit(trainVectorizerArray)
-    sums = transformer.transform(trainVectorizerArray).toarray().sum(1)
-    # print sums
-    # 排序
-    sorted_indices = np.argsort(sums)
-    
-    sFilePath = '/Users/apple/scrapingEnv/weibo-summary/resultData/tf-idf'
-    output_file = ""
-    if not os.path.exists(sFilePath) :
-        os.mkdir(sFilePath)
-    out = open(sFilePath + '/'+ topic_name +'-'+'tf-idf'+'.txt','w+')
+    content = f.readlines()
+    seg = Segmentation.Segmentation()
+    for topic in content:
+        print "\n话题:"
+        topic_name = topic.rstrip()
+        print topic.rstrip()
+        train_set = []
+        line_tweet = []
+        split_line_tweet = []
+        count = 0
+        with open('weiboData/'+topic.rstrip()+'.txt') as data:
+            for tweet in data.readlines():
+                #原始每行数据
+                line_tweet.append(tweet)
+                #中文分词之后的数据
+                result = seg.segment(text=tweet, lower=True)
+                count = count + 1
+                for ss in result.words_no_filter:
+                    split_line_tweet.append(' '.join(ss))
+                if split_line_tweet != []:
+                    train_set.append(split_line_tweet[0])
+                split_line_tweet = []
+        tfidf = transformer.fit_transform(vectorizer.fit_transform(train_set))
+        word = vectorizer.get_feature_names() #所有文本的关键字
+        weight = tfidf.toarray()              #对应的tfidf矩阵
+        sums = weight.sum(1)
+        sorted_indices = np.argsort(sums)
 
-    # print sorted_indices
-    print "best"
-    print sums[sorted_indices[-1]]
-    print train_set[sorted_indices[-1]]
-    print "second best"
-    print sums[sorted_indices[-2]]
-    print train_set[sorted_indices[-2]]
+        # 将结果存入文件
+        sFilePath = '/Users/apple/scrapingEnv/weibo-summary/resultData/tf-idf'
+        output_file = ""
+        if not os.path.exists(sFilePath) :
+            os.mkdir(sFilePath)
+        out = open(sFilePath + '/'+ topic_name +'-'+'tf-idf'+'.txt','w+')
 
-    output_file = output_file + train_set[sorted_indices[-1]]
-    output_file = output_file + train_set[sorted_indices[-2]]
+        # print sorted_indices
+        print "best"
+        print sums[sorted_indices[-1]]
+        print line_tweet[sorted_indices[-1]]
+        print "second best"
+        print sums[sorted_indices[-2]]
+        print line_tweet[sorted_indices[-2]]
 
-    seenWords = stopWords + train_set[sorted_indices[-1]].split(' ')
-    # print seenWords
-    vectorizer2 = CountVectorizer(stop_words = seenWords)
-    trainVectorizerArray = vectorizer2.fit_transform(train_set).toarray()
-    transformer2 = TfidfTransformer()
-    transformer2.fit(trainVectorizerArray)
-    sums = transformer2.transform(trainVectorizerArray).toarray().sum(1)
-    sorted_indices = np.argsort(sums)
-    print "second best with recompiling"
-    print sums[sorted_indices[-1]]
-    print train_set[sorted_indices[-1]]
-    output_file = output_file + train_set[sorted_indices[-1]]
-    out.write(output_file)
-    out.close()
+        output_file = output_file + line_tweet[sorted_indices[-1]]
+        output_file = output_file + line_tweet[sorted_indices[-2]]
+        output_file = output_file + line_tweet[sorted_indices[-3]]
+        
+        out.write(output_file)
+        out.close()
 
