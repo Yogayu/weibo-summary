2cb232821a0d24371da411c3205357ebc1b54897
youxinyu <1015701481@qq.com>
2017-05-24 18:34:30 +0800

    Add Segemnt

--- utilities.py
+++ utilities.py
@@ -26,7 +26,7 @@ def get_train_set(topic):
             for ss in result.words_no_stop_words:
                 split_line_tweet.append(' '.join(ss))
             if split_line_tweet != []:
-                train_set.append(split_line_tweet[0])
+                train_set.append(split_line_tweet[0])#??
             split_line_tweet = []
     return (train_set, line_tweet)
 
@@ -43,3 +43,28 @@ def get_stop_words():
 
 def json_print(item):
     print json.dumps(item,encoding='UTF-8',ensure_ascii=False)
+
+def get_segment_set(file):
+    # 分词之后的数据集
+    train_set = []
+    split_line_tweet = []
+    seg = Segmentation.Segmentation()
+    with open(file) as data:
+        for tweet in data.readlines():
+            # 中文分词之后的数据
+            # print tweet
+            result = seg.segment(text=tweet, lower=True)
+            # json_print(result)
+            for ss in result.words_no_stop_words:
+                # json_print(ss)
+                # print len(ss)
+                split_line_tweet.append(' '.join(ss))
+            # json_print(split_line_tweet)
+            line_string = ""
+            if split_line_tweet != []:
+                for i in xrange(0,len(split_line_tweet)):
+                    line_string += split_line_tweet[i]
+            # json_print(line_string)
+            train_set.append(line_string)
+            split_line_tweet = []
+    return train_set
